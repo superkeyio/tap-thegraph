09fc0f32b3e9f7619f98436168bb0ba9aac8256a
Matt Evenson <mevenson@usc.edu>
2022-05-21 21:50:53 -0700

    remove unnecessary stuff

--- tap_thegraph/streams.py
+++ tap_thegraph/streams.py
@@ -8,34 +8,6 @@ from singer_sdk import typing as th  # JSON Schema typing helpers
 
 from tap_thegraph.client import TheGraphStream
 from graphql import GraphQLEnumType, GraphQLInterfaceType, GraphQLNonNull, GraphQLOutputType, GraphQLScalarType, GraphQLUnionType, GraphQLWrappingType, get_introspection_query, GraphQLObjectType
-
-# TODO: Delete this is if not using json files for schema definition
-SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
-# TODO: - Override `UsersStream` and `GroupsStream` with your own stream definition.
-#       - Copy-paste as many times as needed to create multiple stream types.
-
-# TODO: heavy lifting here
-def convert_graphql_output_type_to_schema(graphql_output_type: GraphQLOutputType):
-    # account for lists somehow
-    if isinstance(graphql_output_type, GraphQLScalarType):
-        print('Scalar', graphql_output_type)
-        return dict(wrapped=th.StringType)
-    elif isinstance(graphql_output_type, GraphQLObjectType) or isinstance(graphql_output_type, GraphQLInterfaceType):
-        print('Object', graphql_output_type)
-        properties: List[th.Property] = []
-        for name, field in graphql_output_type.fields.items():
-            properties.append(th.Property(name, **convert_graphql_output_type_to_schema(field.type)))
-        return th.ObjectType(*properties)
-    elif isinstance(graphql_output_type, GraphQLEnumType):
-        print('Enum', graphql_output_type)
-        pass
-    elif isinstance(graphql_output_type, GraphQLUnionType):
-        print('Union', graphql_output_type)
-        pass
-    elif isinstance(graphql_output_type, GraphQLNonNull):
-        print('Wrapping', graphql_output_type)
-        return dict(wrapped=th.StringType, required=True)
-
 class EntityStream(TheGraphStream):
     entity: Union[GraphQLObjectType, GraphQLInterfaceType]
         
@@ -47,13 +19,14 @@ class EntityStream(TheGraphStream):
 
     def __init__(self, *args, **kwargs):
         self.entity = kwargs.pop('entity')
-        self.replication_key = kwargs.pop('replication_key')
+        self.replication_key = kwargs.pop('replication_key') # timestamp
         super().__init__(*args, **kwargs)
 
     @property
     def schema(self) -> dict:
-        r: th.ObjectType = convert_graphql_output_type_to_schema(self.entity)
-        return r.to_dict()
+        # TODO: convert GraphQL schema to JSON schema and filter
+        return {}
+
 
     @property
     def query(self) -> str:
