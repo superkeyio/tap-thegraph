ffc61e546386f2335b9eba2801223929be483148
Matt Evenson <mevenson@usc.edu>
2022-05-21 22:10:52 -0700

    get rid of bs

--- subgraph-to-json-schema/src/index.ts
+++ subgraph-to-json-schema/src/index.ts
@@ -1,1 +1,16 @@
-export {run} from '@oclif/core'
+import {Command, flush, Errors} from '@oclif/core'
+
+export default class SubgraphToJSONSchema extends Command {
+  static description = 'Say hello'
+
+  static args = [{name: 'subgraphUrl', description: 'URL of the the subgraph', required: true}]
+
+  async run(): Promise<void> {
+    const {args} = await this.parse(SubgraphToJSONSchema)
+
+    console.log(args.subgraphUrl)
+  }
+}
+
+// Start the CLI
+SubgraphToJSONSchema.run().then(flush, Errors.handle)
